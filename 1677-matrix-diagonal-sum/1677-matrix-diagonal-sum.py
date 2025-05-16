c1dcb2c7a457b1bd904b99a2f6fa2aaa44e3fc96
guru741 <138288541+guru741@users.noreply.github.com>
2025-05-16 10:39:10 +0530

    Time: 0 ms (100%), Space: 12.4 MB (96.05%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
@@ -1,8 +1,20 @@
 class Solution(object):
     def diagonalSum(self, mat):
+        n = len(mat)
         sum_ele = 0
-        for i in range(len(mat)):
-            for j in range(len(mat)):
-                if i == j or (i + j) == len(mat) - 1:
-                    sum_ele += mat[i][j]
-        return sum_ele
+        for i in range(n):
+            sum_ele += mat[i][i]
+            sum_ele += mat[i][n - 1 - i]
+        
+        if n % 2 == 1:
+            sum_ele -= mat[n / 2][n / 2]
+
+        return sum_ele 
+
+
+        # sum_ele = 0
+        # for i in range(len(mat)):
+        #     for j in range(len(mat)):
+        #         if i == j or (i + j) == len(mat) - 1:
+        #             sum_ele += mat[i][j]
+        # return sum_ele
