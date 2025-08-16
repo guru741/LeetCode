f69058b477b94da49e88cbed084185822922b58a
guru741 <138288541+guru741@users.noreply.github.com>
2025-08-16 22:28:27 +0530

    Time: 1300 ms (77.34%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,9 @@
+
+select s.name from SalesPerson s
+where s.sales_id not in (
+    select distinct o.sales_id
+    from Orders o 
+    join company c
+    on o.com_id = c.com_id 
+    where c.name = 'RED'
+);
