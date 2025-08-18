289af8631f7857a8abf97887177552e899251e17
guru741 <138288541+guru741@users.noreply.github.com>
2025-08-18 22:22:45 +0530

    Time: 583 ms (22.01%), Space: 0B (100%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.sql
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -1,4 +1,6 @@
-select p.project_id,round(avg(e.experience_years),2) as average_years
+select p.project_id,
+round(avg(e.experience_years),2) 
+as average_years
 from Project p
 join Employee e
 on p.employee_id = e.employee_id
