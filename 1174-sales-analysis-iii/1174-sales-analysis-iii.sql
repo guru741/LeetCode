ffb83cae71c4a363cf53923740e8fb65f4387c4e
guru741 <138288541+guru741@users.noreply.github.com>
2025-08-19 13:19:32 +0530

    Time: 1175 ms (51.56%), Space: 0B (100%) - LeetHub

---
+++ 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
@@ -0,0 +1,14 @@
+select distinct p.product_id,p.product_name 
+from Product p
+where exists (
+    select 1
+    from Sales s
+    where s.product_id = p.product_id
+        and s.sale_date between '2019-01-01' and '2019-03-31' 
+)
+and not exists(
+    select 1
+    from Sales s
+    where s.product_id = p.product_id
+        and (s.sale_date < '2019-01-01' or s.sale_date > '2019-03-31')
+);
