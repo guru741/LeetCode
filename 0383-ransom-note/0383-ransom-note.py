56fa2cc19394d70b9fcce6e963cb2a7b61e75b01
guru741 <138288541+guru741@users.noreply.github.com>
2025-10-06 20:40:21 +0530

    Time: 43 ms (36.86%), Space: 12.7 MB (78.96%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,5 +1,12 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        ransom,maga = Counter(ransomNote),Counter(magazine)
-        return not(ransom - maga)
-        
+        # ransom,maga = Counter(ransomNote),Counter(magazine)
+        # return not(ransom - maga)
+        freq = [0] * 26
+        for i in magazine:
+            freq[ord(i) - ord('a')] += 1
+        for i in ransomNote:
+            freq[ord(i) - ord('a')] -= 1
+            if freq[ord(i) - ord('a')] < 0:
+                return False
+        return True
